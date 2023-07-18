62c6ee089474a6b96e62d52f8ce406a44b4fd6f2
Oksanarym40 <135235310+Oksanarym40@users.noreply.github.com>
2023-07-18 17:22:42 -0400

    Update Main.java
    Removed serialNumber and distanceFromSun. Thanks

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,59 +1,49 @@
 package org.example;
 
-public class Main {
-    public enum SolarSystem {
-        MERCURY(1, 0, 0, null),
-        VENUS(2, 1, 40, MERCURY),
-        EARTH(3, 2, 70, VENUS),
-        MARS(4, 3, 110, EARTH),
-        JUPITER(5, 4, 160, MARS),
-        SATURN(6, 5, 220, JUPITER),
-        URANUS(7, 6, 280, SATURN),
-        NEPTUNE(8, 7, 350, URANUS),
-        PLUTO(9, 8, 430, NEPTUNE);
+public enum SolarSystem {
+    MERCURY(0, 0, null),
+    VENUS(1, 40, MERCURY),
+    EARTH(2, 70, VENUS),
+    MARS(3, 110, EARTH),
+    JUPITER(4, 160, MARS),
+    SATURN(5, 220, JUPITER),
+    URANUS(6, 280, SATURN),
+    NEPTUNE(7, 350, URANUS),
+    PLUTO(8, 430, NEPTUNE);
 
-        private int serialNumber;
-        private int distanceFromPrevious;
-        private int distanceFromSun;
-        private int radius;
-        private SolarSystem previousPlanet;
-        private SolarSystem nextPlanet;
+    private int distanceFromPrevious;
+    private int radius;
+    private SolarSystem previousPlanet;
+    private SolarSystem nextPlanet;
 
-        SolarSystem(int serialNumber, int distanceFromPrevious, int distanceFromSun, SolarSystem previous) {
-            this.serialNumber = serialNumber;
-            this.distanceFromPrevious = distanceFromPrevious;
-            if (previous != null) {
-                this.distanceFromSun = previous.distanceFromSun + distanceFromSun;
-                this.previousPlanet = previous;
-                previous.nextPlanet = this;
-            } else {
-                this.distanceFromSun = distanceFromSun;
-            }
-            this.radius = distanceFromSun / 10;  
+    SolarSystem(int distanceFromPrevious, SolarSystem previous) {
+        this.distanceFromPrevious = distanceFromPrevious;
+        if (previous != null) {
+            this.previousPlanet = previous;
+            previous.nextPlanet = this;
         }
+        this.radius = distanceFromPrevious / 10;  // Just an example calculation for radius based on distance
+    }
 
-        public int getSerialNumber() {
-            return serialNumber;
-        }
+    public int getDistanceFromPrevious() {
+        return distanceFromPrevious;
+    }
 
-        public int getDistanceFromPrevious() {
-            return distanceFromPrevious;
-        }
+    public int getRadius() {
+        return radius;
+    }
 
-        public int getDistanceFromSun() {
-            return distanceFromSun;
-        }
+    public SolarSystem getPreviousPlanet() {
+        return previousPlanet;
+    }
 
-        public int getRadius() {
-            return radius;
-        }
-
-        public SolarSystem getPreviousPlanet() {
-            return previousPlanet;
-        }
-
-        public SolarSystem getNextPlanet() {
-            return nextPlanet;
-        }
+    public SolarSystem getNextPlanet() {
+        return nextPlanet;
     }
 }
+
+
+
+
+
+
